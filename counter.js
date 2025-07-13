ab6805a1d12c76e5731b3fcfab85f692a31ec1f0
MuhammadWaqas2772 <chwaqas130530@gmail.com>
2025-07-13 01:00:30 -0700

    Initial commit

---
+++ counter.js
@@ -0,0 +1,16 @@
+let count=0;
+function updateCounter(){
+document.getElementById("counter").textContent=count;
+};
+function increment(){
+    count++;
+    updateCounter();
+}
+function decrement(){
+    count--;
+    updateCounter();
+}
+function reset(){
+    count=0;
+    updateCounter();
+}
